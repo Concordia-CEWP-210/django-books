a0ca0e13a276f9bf99a40e354b89c60befb4713f
PranavBhatia <bhatiapranav1996@gmail.com>
2023-11-08 21:43:14 -0500

    DB

--- books/urls.py
+++ books/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index), # books/
-    path('<slug:book_slug>', views.book_details),  # books/my-book-name
+    path('<slug:book_slug>', views.book_details),
 ]
--- books/views.py
+++ books/views.py
@@ -1,35 +1,18 @@
 from django.shortcuts import render
+from .models import Book
 
 # Create your views here.
+
+
 def index(request):
-    books = [
-        {
-            'name': 'Hello Beautiful',
-            'author': 'Ann Napolitano',
-            'coverImage': 'https://m.media-amazon.com/images/I/51YGx8qVVXL.jpg'
-        },
-        {
-            'name': 'Fourth Wing',
-            'author': 'Rebecca Yarros',
-            'coverImage': 'https://cdn.kobo.com/book-images/11b52103-2310-4db2-bd9d-3a416cdbc875/1200/1200/False/fourth-wing.jpg'
-        },
-    ]
+    books = Book.objects.all()
     return render(request, 'books/index.html', {
-        'show_books': True,
         'books': books
     })
 
 
 def book_details(request, book_slug):
-    print(book_slug)
-    selectedBook = {
-        'name': 'Hello Beautiful',
-        'author': 'Ann Napolitano',
-        'coverImage': 'https://m.media-amazon.com/images/I/51YGx8qVVXL.jpg',
-        'numPages': 255,
-        'desctipion': 'The best book ever',
-        'slug': 'hello-beautiful'
-    }
+    selectedBook = Book.objects.get(slug=book_slug)
     return render(request, 'books/book-details.html', {
         'book': selectedBook
     })
